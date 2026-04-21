3fde5f62b6a5aa036dc4b8654eab1b1771c29f41
justAbing <740524463@qq.com>
2026-04-22 03:09:00 +0800

    Update duolingo.js

--- duolingo.js
+++ duolingo.js
@@ -1,8 +1,6 @@
 [rewrite_local]
-^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\?fields=subscriberLevel url script-response-body https://github.com/justAbing/testdo/blob/0eb42f2f06aa1bd8fb28de581a9429954479e8cf/duolingoleve.js
-, tag=订购等级
-^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\/available-features.*$ url script-response-body https://github.com/justAbing/testdo/blob/0eb42f2f06aa1bd8fb28de581a9429954479e8cf/duolingo.js, tag=订购配置
-^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/\d{4}-\d{1,2}-\d{1,2}\/batch url script-response-body https://github.com/justAbing/testdo/blob/0eb42f2f06aa1bd8fb28de581a9429954479e8cf/batch.js
-, tag=订购信息
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\?fields=subscriberLevel url script-response-body https://raw.githubusercontent.com/justAbing/testdo/refs/heads/main/tota.js, tag=订购等级
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/.*\/users\/.*\/available-features.*$ url script-response-body https://raw.githubusercontent.com/justAbing/testdo/refs/heads/main/tota.js, tag=订购配置
+^https?:\/\/(?:ios-api-[^.]+|api[^.]+)\.duolingo\.[a-z.]+\/\d{4}-\d{1,2}-\d{1,2}\/batch url script-response-body https://raw.githubusercontent.com/justAbing/testdo/refs/heads/main/tota.js, tag=订购信息
 [mitm]
 hostname = ios-api-*.duolingo.*, api*.duolingo.*
